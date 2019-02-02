8f7a062e9a288de8444f7609c3e676921bfdba97
James Messinger <james.messinger@gmail.com>
2019-02-02 10:33:06 -0600

    Simplified the tests and made them more resilient to differences in environments

--- test/utils/fixtures.js
+++ test/utils/fixtures.js
@@ -1,32 +1,22 @@
 "use strict";
 
-const fs = require("fs");
 const nodePath = require("path");
 
 const fixturesDir = nodePath.join(__dirname, "..", "fixtures") + nodePath.sep;
-const escapedFixturesDir = fixturesDir.replace(/\\/g, "\\\\");
 
 module.exports = {
+  path: fixturesDir,
+  escapedPath: fixturesDir.replace(/\\/g, "\\\\"),
+
   dist: {
     path: nodePath.join(fixturesDir, "dist"),
   },
 
-  "esm-sample.js": fileInfo(nodePath.join(fixturesDir, "esm-sample.js")),
-  "esm-sample.instrumented.js": fileInfo(nodePath.join(fixturesDir, "esm-sample.instrumented.js")),
-  "esm-sample.instrumented.js.map": fileInfo(nodePath.join(fixturesDir, "esm-sample.instrumented.js.map")),
+  "esm-sample.js": {
+    path: nodePath.join(fixturesDir, "esm-sample.js"),
+  },
 
-  "ts-sample.js": fileInfo(nodePath.join(fixturesDir, "ts-sample.js")),
-  "ts-sample.instrumented.js": fileInfo(nodePath.join(fixturesDir, "ts-sample.instrumented.js")),
-  "ts-sample.instrumented.js.map": fileInfo(nodePath.join(fixturesDir, "ts-sample.instrumented.js.map")),
+  "ts-sample.js": {
+    path: nodePath.join(fixturesDir, "ts-sample.js"),
+  },
 };
-
-function fileInfo (path) {
-  let contents = fs.readFileSync(path, "utf8").trim();
-  contents = contents.replace(/%ROOT_PATH%/g, escapedFixturesDir);
-
-  if (path.endsWith(".map")) {
-    contents = JSON.parse(contents);
-  }
-
-  return { path, contents };
-}
